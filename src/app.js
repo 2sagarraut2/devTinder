cf8c37e5d124557ae93fd2907f8df38f7ddeca0e
Sagar Raut <2sagarraut2@gmail.com>
2025-09-08 16:36:33 +0530

    Databse connected, User model created

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 const express = require("express");
-
+require("dotenv").config();
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.use("/test", (req, res) => {
-  res.send("Hello from server!");
+app.post("/signup", async (req, res) => {
+  // creating new instance of user model
+  const user = new User({
+    firstName: "Komal",
+    lastName: "Raut",
+    emailId: "komal@gmail.com",
+    password: "Komal@123",
+  });
+
+  try {
+    await user.save();
+    res.json({ message: "User saved successfully!" });
+  } catch (err) {
+    res.status(400).send("Error occured while saving user " + err.message);
+  }
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello!");
-});
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established!");
+    app.listen(7777, () => {
+      console.log("Server started on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Database connection failed");
+    console.error("Error name:", err.name);
+    console.error("Error message:", err.message);
+    console.error("Full error:", err);
+  });
