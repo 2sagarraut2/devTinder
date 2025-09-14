0a87ae1a17b77f6928c66bdb5b74c9b629e092dd
Sagar Raut <2sagarraut2@gmail.com>
2025-09-14 12:02:15 +0530

    /user/connections API added

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,8 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 // Get all pending connection requests for loggedInUser
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -13,25 +15,42 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "photoUrl",
-      "age",
-      "gender",
-      "about",
-      "skills",
-    ]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     return res.json({
       message: "Connection requests fetched successfully",
       data: connectionRequests,
     });
   } catch (err) {
+    console.log(err);
     res.status(400).send({
       error: "An unexpected error occurred. Please try again later. " + err,
     });
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    const data = connections.map((row) => row.fromUserId);
+
+    return res.json({
+      message: "Retrieved connections successfully",
+      data,
+    });
+  } catch (err) {
     console.log(err);
+    res.status(400).send({
+      error: "An unexpected error occurred. Please try again later. " + err,
+    });
   }
 });
 
